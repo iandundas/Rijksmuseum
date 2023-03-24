3e2a3e34a14343002031dcc96928bf18d11c7916
Ian Dundas <1131967+iandundas@users.noreply.github.com>
2023-03-24 16:44:46 +0100

    Sketch out SPM modules

---
+++ Feature_OverviewScreen/Tests/Feature_OverviewScreenTests/Feature_OverviewScreenTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import Feature_OverviewScreen
+
+final class Feature_OverviewScreenTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(Feature_OverviewScreen().text, "Hello, World!")
+    }
+}
